2ab72b8ee3b72dd45430a064497bd8c9b2aae943
Your Name <you@example.com>
2025-02-28 02:37:57 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/v8/DTH-YOLO.yaml')
+    model.train(data='ultralytics/cfg/datasets/mousehole.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=200,
+                batch=20,
+                workers=10,
+                device='0',
+                optimizer='SGD',
+                # resume='True',
+                project='runs/train',
+                name='DTH-YOLO',
+                )
+
